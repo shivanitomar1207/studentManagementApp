b3229437e0205c0f12d835593b951ecf64377288
jyoti tomer <jyotitomer@jyotis-MacBook-Air.local>
2025-08-09 22:49:45 +0530

    debug is false now

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*5-+x%@k@kme!yw^r@x@3qct%8kze#nfx5!jcpc==_9lo%b073'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['studentmanagementapp-1-kicn.onrender.com']
 
